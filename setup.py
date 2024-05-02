d6cedc282db75f0466c5dd88c545aa83b32bea2d
lidi10 <lidi10@lidi10deMacBook-Pro.local>
2024-05-02 21:39:57 +0800

    0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sqlvana',
-    version='0.1.2',
+    version='0.1.3',
     package_dir={'': 'src'}, 
     packages=find_packages(where='src'),
     install_requires=[
